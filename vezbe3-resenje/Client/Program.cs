4273155deb4674042a24febaef177b9186310320
Milenko98 <61869042+Milenko98@users.noreply.github.com>
2020-05-18 14:04:29 +0200

    Ispis poruke na klijentu

---
+++ vezbe3-resenje/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void ispis()
+            {
+                Console.WriteLine("da");
+            }
+            ispis();
+        }
+    }
+}
